212595001be018387df3e30444b164b2199304a9
Jorge Atienza <158172073+Sala2-20@users.noreply.github.com>
2025-03-05 12:33:11 +0100

    Update app.py

--- src/app.py
+++ src/app.py
@@ -250,4 +250,7 @@ def buscarUsu(nombre, contrasenia):
             return {"encontrado": True, "id": obj["id"]}
         
     return {"encontrado": False}
-
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
